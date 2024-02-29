04d72f51069d7a9b5c6843b924f639aa015c02cc
P13745 <148933327+P13745@users.noreply.github.com>
2024-02-29 17:39:51 +0900

    fix

--- MyApplication/app/src/main/java/com/example/myapplication/Screen.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/Screen.kt
@@ -13,12 +13,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.ButtonColors
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -38,8 +40,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
@@ -500,52 +504,60 @@ fun EachVertex(
         ){
             Text( (id + 1).toString())
 
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
 
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Row(
+                modifier = Modifier.clip(shape = RoundedCornerShape(16.dp))
             ) {
                 Button(
-                    modifier = Modifier.height(32.dp),
-                    onClick = { move(id,"UP", movingSpeed) },
+                    modifier = Modifier
+                        .width(50.dp)
+                        .height(40.dp),
+                    shape = RectangleShape,
+                    onClick = { move(id, "LEFT", movingSpeed) },
+                    content = { Text("←") },
+                    enabled = !loadingOrSaving
+                )
+                Button(
+                    modifier = Modifier
+                        .width(50.dp)
+                        .height(40.dp),
+                    shape = RectangleShape,
+                    onClick = { move(id, "UP", movingSpeed) },
                     content = { Text("↑") },
                     enabled = !loadingOrSaving
                 )
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceEvenly
-                ){
-                    Button(
-                        modifier = Modifier
-                            .height(32.dp)
-                            .width(60.dp),
-                        onClick = { move(id,"LEFT", movingSpeed) },
-                        content = { Text("←") },
-                        enabled = !loadingOrSaving
-                    )
-                    Button(
-                        modifier = Modifier
-                            .height(32.dp)
-                            .width(60.dp),
-                        onClick = { move(id,"RIGHT", movingSpeed) },
-                        content = { Text("→") },
-                        enabled = !loadingOrSaving
-                    )
-                }
                 Button(
-                    modifier = Modifier.height(32.dp),
-                    onClick = { move(id,"DOWN", movingSpeed) },
+                    modifier = Modifier
+                        .width(50.dp)
+                        .height(40.dp),
+                    shape = RectangleShape,
+                    onClick = { move(id, "DOWN", movingSpeed) },
                     content = { Text("↓") },
                     enabled = !loadingOrSaving
                 )
-
+                Button(
+                    modifier = Modifier
+                        .width(50.dp)
+                        .height(40.dp),
+                    shape = RectangleShape,
+                    onClick = { move(id, "RIGHT", movingSpeed) },
+                    content = { Text("→") },
+                    enabled = !loadingOrSaving
+                )
             }
 
 
+
+
             Button(
+                modifier = Modifier
+                    .height(40.dp),
                 onClick = { deleteVertex(id) },//====================
                 content = { Text("Delete") },
-                enabled = !loadingOrSaving
+                enabled = !loadingOrSaving,
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
             )
 
 
